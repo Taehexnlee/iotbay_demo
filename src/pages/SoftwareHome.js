5b22807b9d767e8f0344bb274a99b8afcecd2738
eh2004 <evan.a.haines@student.uts.edu.au>
2024-04-30 13:57:27 +1000

    product pages

--- src/pages/SoftwareHome.js
+++ src/pages/SoftwareHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import IoTBayLogo from '../pages/Images/IoTBay.png';
 
 export default function SoftwareHome() {
   return (
@@ -17,6 +18,36 @@ export default function SoftwareHome() {
             <button type="submit">Search</button>
             </form>
           </div>
+          <div class="product-grid">
+        <div class="product">
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+            </div> 
+        </div>
+        <div class="product">
+            {/* <img> src="product1.jpg" alt="Product 2"</img> */}
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+            </div> 
+        </div>
+        <div class="product">
+            {/* <img> src="product1.jpg" alt="Product 3"</img> */}
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+            </div> 
+        </div>
+        <div class="product">
+            {/* <img> src="product1.jpg" alt="Product 4"</img> */}
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+            </div> 
+        </div>
+        </div>
       </main>
     </div>
   </div>
