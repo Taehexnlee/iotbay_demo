f3bbdd80030d4a41657c5ea4b6d9aee2a130bc49
eh2004 <evan.a.haines@student.uts.edu.au>
2024-04-30 14:06:35 +1000

    semi-final software pages

--- src/pages/SoftwareHome.js
+++ src/pages/SoftwareHome.js
@@ -27,25 +27,39 @@ export default function SoftwareHome() {
             </div> 
         </div>
         <div class="product">
-            {/* <img> src="product1.jpg" alt="Product 2"</img> */}
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
             <div class="product-info">
                 <h3>Product Name 1</h3>
                 <p>$19.99</p>
             </div> 
         </div>
         <div class="product">
-            {/* <img> src="product1.jpg" alt="Product 3"</img> */}
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
             <div class="product-info">
                 <h3>Product Name 1</h3>
                 <p>$19.99</p>
             </div> 
         </div>
         <div class="product">
-            {/* <img> src="product1.jpg" alt="Product 4"</img> */}
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
             <div class="product-info">
                 <h3>Product Name 1</h3>
                 <p>$19.99</p>
-            </div> 
+        </div>
+        </div>
+        <div class="product">
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+        </div>
+        </div>
+        <div class="product">
+            <img src={IoTBayLogo} alt="IoTBay" class="logo-image"></img>
+            <div class="product-info">
+                <h3>Product Name 1</h3>
+                <p>$19.99</p>
+        </div>
         </div>
         </div>
       </main>
