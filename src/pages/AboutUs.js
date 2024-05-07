1f3db23c65ab80a934a555b2da169b798cdf083d
aschente98 <jjs980919@gmail.com>
2024-05-07 13:49:25 +1000

    Create AboutUs.js

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function AboutUs() {
+  return (
+    <div className='container'>
+      <div className='py-4'>
+        <header>
+         
+        </header>
+
+        <main>
+          <p class="hometext">About us</p>
+        </main>
+
+      </div>
+    </div>
+  )
+}
