44b6fb7ccce8d7a44d921a6751b07253c5782e68
leehwan0218 <thhoney218@gmail.com>
2024-05-13 22:21:13 +1000

    h

--- src/shipping/AddShipping.js
+++ src/shipping/AddShipping.js
@@ -10,7 +10,7 @@ export default function AddShipping() {
         street: "",
         city: "",
         state: "",
-        zipCode: "",
+        zipCode: ""
     });
     const [error, setError] = useState(false);
 
