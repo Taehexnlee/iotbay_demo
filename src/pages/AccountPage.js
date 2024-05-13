f4f81ab359a19ffce4d8da2932be8f655e9311f5
aschente98 <jjs980919@gmail.com>
2024-05-13 16:50:31 +1000

    z

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -6,7 +6,7 @@ const AccountPage = () => {
     <div>
       <h2>Account</h2>
         <Link to="/edit" className="content-display1">Edit Account</Link>
-        <Link to="/view" className="content-display2">My Account Detail</Link>
+        <Link to="/view" className="content-display2">My Account Details</Link>
         <Link to="/paymentHistory" className="content-display3">Payment History</Link>
         <Link to="/logs" className="content-display4">View Logs</Link>
     </div>
